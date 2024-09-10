2776cdc62d75a0cf01f27c0ec83aa0dc1530867e
Csipkés Gergő <csipkesgeri@hotmail.com>
2024-09-10 08:28:36 +0200

    fine tune resources

--- nestjs-vehicle-register/src/app/app.module.ts
+++ nestjs-vehicle-register/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { LoggerMiddleware } from 'src/common/middlewares/logger.middleware';
+// import { LoggerMiddleware } from 'src/common/middlewares/logger.middleware';
 import { DatabaseModule } from 'src/cores/database/database.module';
 import { VehicleModule } from 'src/features/vehicle/vehicle.module';
 
@@ -14,9 +14,9 @@ import { VehicleModule } from 'src/features/vehicle/vehicle.module';
   ],
 })
 export class AppModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(LoggerMiddleware)
-      .forRoutes({ path: '*', method: RequestMethod.ALL });
-  }
+  //   configure(consumer: MiddlewareConsumer) {
+  //     consumer
+  //       .apply(LoggerMiddleware)
+  //       .forRoutes({ path: '*', method: RequestMethod.ALL });
+  //   }
 }
