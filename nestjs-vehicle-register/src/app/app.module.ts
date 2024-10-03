db8e2c2ee08e2b9419f50d4828167894b6dd41be
Csipkes Gergo <csipkesgeri@gmail.com>
2024-10-03 09:45:04 +0200

    redis cache a get uuid endpointra

--- nestjs-vehicle-register/src/app/app.module.ts
+++ nestjs-vehicle-register/src/app/app.module.ts
@@ -1,13 +1,22 @@
+import type { RedisClientOptions } from 'redis';
+import * as redisStore from 'cache-manager-redis-store';
 import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
 // import { LoggerMiddleware } from 'src/common/middlewares/logger.middleware';
 import { DatabaseModule } from '../cores/database/database.module';
 import { VehicleModule } from '../features/vehicle/vehicle.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    CacheModule.register<RedisClientOptions>({
+      isGlobal: true,
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: parseInt(process.env.REDIS_PORT),
     }),
     DatabaseModule,
     VehicleModule,
--- nestjs-vehicle-register/src/features/vehicle/controllers/vehicle.controller.ts
+++ nestjs-vehicle-register/src/features/vehicle/controllers/vehicle.controller.ts
@@ -1,4 +1,5 @@
 import { Response } from 'express';
+import { Cache } from 'cache-manager';
 import {
   Controller,
   Get,
@@ -8,13 +9,20 @@ import {
   Res,
   HttpStatus,
   Query,
+  UseInterceptors,
+  Inject,
 } from '@nestjs/common';
 import { VehicleService } from '../services/vehicle.service';
 import { CreateVehicleDto } from '../dto/create-vehicle.dto';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Vehicle } from '../entities/vehicle.entity';
 
 @Controller()
 export class VehicleController {
-  constructor(private readonly vehicleService: VehicleService) {}
+  constructor(
+    private readonly vehicleService: VehicleService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
 
   @Post('jarmuvek')
   async create(
@@ -23,42 +31,45 @@ export class VehicleController {
   ) {
     try {
       const vehicle = await this.vehicleService.create(createVehicleDto);
-      res
-        .status(HttpStatus.CREATED)
-        .header('Location', `/jarmuvek/${vehicle.uuid}`)
-        .send();
+      // Store the vehicle in the cache
+      await this.cacheManager.set(vehicle.uuid, vehicle, 1600);
+      res.status(201).header('Location', `/jarmuvek/${vehicle.uuid}`).send();
     } catch (error) {
-      res.status(HttpStatus.BAD_REQUEST).send({ message: error.message });
+      console.error(error);
+      res.status(400).send({ message: error.message });
     }
   }
 
   @Get('kereses')
   async findByText(@Query('q') text: string, @Res() res: Response) {
     if (!text) {
-      return res
-        .status(HttpStatus.BAD_REQUEST)
-        .send('A "q" paraméter kötelező.');
+      return res.status(400).send('A "q" paraméter kötelező.');
     }
     const vehicles = await this.vehicleService.findByText(text);
-    res.status(HttpStatus.OK).send(vehicles);
+    res.status(200).send(vehicles);
   }
 
   @Get('jarmuvek/:uuid')
   async findByUuid(@Param('uuid') uuid: string, @Res() res: Response) {
-    const vehicle = await this.vehicleService.findByUuid(uuid);
-    if (vehicle) {
-      res.status(HttpStatus.OK).send(vehicle);
+    // Find vehicle by UUID in cache
+    const cachedVehicle = await this.cacheManager.get<Vehicle>(uuid);
+    if (cachedVehicle) {
+      return res.status(200).send(cachedVehicle);
     } else {
-      res.status(HttpStatus.NOT_FOUND).send();
+      res.status(404).send();
     }
+
+    // const vehicle = await this.vehicleService.findByUuid(uuid);
+    // if (vehicle) {
+    //   res.status(200).send(vehicle);
+    // } else {
+    //   res.status(404).send();
+    // }
   }
 
   @Get('jarmuvek')
   async countAll(@Res() res: Response) {
     const count = await this.vehicleService.countAll();
-    res
-      .status(HttpStatus.OK)
-      .header('Content-Type', 'text/plain')
-      .send(count.toString());
+    res.status(200).header('Content-Type', 'text/plain').send(count.toString());
   }
 }
